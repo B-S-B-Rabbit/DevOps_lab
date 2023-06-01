2ce558a97517260af28742b323f98feac8559d32
B-S-B-Rabbit <mystery.knight113@gmail.com>
2023-06-01 16:20:45 +0300

    add pytests, build.yml 0.8

--- main.py
+++ main.py
@@ -1,41 +1,39 @@
 import json
 import os
 
-TODO_FILE = "todo.json"
 
-
-def load_tasks():
+def load_tasks(todo_file):
     tasks = []
-    if os.path.exists(TODO_FILE):
-        with open(TODO_FILE, "r") as f:
+    if os.path.exists(todo_file):
+        with open(todo_file, "r") as f:
             tasks = json.load(f)
     return tasks
 
 
-def save_tasks(tasks):
-    with open(TODO_FILE, "w") as f:
+def save_tasks(tasks, todo_file):
+    with open(todo_file, "w") as f:
         json.dump(tasks, f)
 
 
-def add_task(task):
-    tasks = load_tasks()
+def add_task(task, todo_file):
+    tasks = load_tasks(todo_file)
     tasks.append(task)
-    save_tasks(tasks)
+    save_tasks(tasks, todo_file)
 
 
-def remove_task(task):
-    tasks = load_tasks()
+def remove_task(task, todo_file):
+    tasks = load_tasks(todo_file)
     tasks.pop(int(task) - 1)
-    save_tasks(tasks)
+    save_tasks(tasks, todo_file)
 
 
-def list_tasks():
-    tasks = load_tasks()
+def list_tasks(todo_file):
+    tasks = load_tasks(todo_file)
     for index, task in enumerate(tasks, start=1):
         print(f"{index}. {task}")
 
 
-def main():
+def main(todo_file):
     print("Todo List")
     print("1. Add task")
     print("2. Remove task")
@@ -46,21 +44,23 @@ def main():
         choice = input("Enter your choice: ")
         if choice == "1":
             task = input("Enter task: ")
-            add_task(task)
+            add_task(task, todo_file)
             print("The task was successfully added")
         elif choice == "2":
             print("Here your list of tasks:")
-            list_tasks()
+            list_tasks(todo_file)
             task = input("Enter the number of task to remove from this list: ")
-            remove_task(task)
+            remove_task(task,todo_file)
             print("The task was successfully removed")
         elif choice == "3":
-            list_tasks()
+            list_tasks(todo_file)
         elif choice == "4":
+            print("Exit")
             break
         else:
             print("Invalid choice. Try again.")
 
 
 if __name__ == "__main__":
-    main()
+    td = "todo.json"
+    main(td)
---
+++ test_todo.py
@@ -0,0 +1,104 @@
+import json
+import os
+import pytest
+
+from main import load_tasks, save_tasks, add_task, remove_task, list_tasks
+
+
+@pytest.fixture
+def todo_file(tmpdir):
+    """Фикстура для создания временного файла todo.json."""
+    todo_file = tmpdir.join("todo1.json")
+    return str(todo_file)
+
+
+def test_load_tasks(todo_file):
+    """Тест для функции load_tasks()."""
+    tasks = load_tasks(todo_file)
+    assert tasks == []
+
+    # Создаем временный файл с задачами
+    tasks_data = ["Task 1", "Task 2", "Task 3"]
+    with open(todo_file, "w") as f:
+        json.dump(tasks_data, f)
+
+    # Загружаем задачи из временного файла
+    tasks = load_tasks(todo_file)
+    assert tasks == tasks_data
+
+
+def test_save_tasks(todo_file):
+    """Тест для функции save_tasks()."""
+    tasks_data = ["Task 1", "Task 2", "Task 3"]
+
+    # Сохраняем задачи во временный файл
+    save_tasks(tasks_data, todo_file,)
+
+    # Проверяем, что данные сохранены корректно
+    with open(todo_file, "r") as f:
+        saved_tasks = json.load(f)
+
+    assert saved_tasks == tasks_data
+
+
+def test_add_task(todo_file):
+    """Тест для функции add_task()."""
+    task = "New Task"
+
+    # Добавляем задачу
+    add_task(task, todo_file)
+
+    # Проверяем, что задача добавлена
+    tasks = load_tasks(todo_file)
+    assert task in tasks
+
+
+def test_remove_task(todo_file):
+    """Тест для функции remove_task()."""
+    tasks_data = ["Task 1", "Task 2", "Task 3"]
+
+    # Создаем временный файл с задачами
+    with open(todo_file, "w") as f:
+        json.dump(tasks_data, f)
+
+    task_to_remove = 2
+
+    # Удаляем задачу
+    remove_task(task_to_remove, todo_file)
+
+    # Проверяем, что задача удалена
+    tasks = load_tasks(todo_file)
+    assert task_to_remove not in tasks
+
+
+def test_list_tasks(todo_file, capsys):
+    """Тест для функции list_tasks()."""
+    tasks_data = ["Task 1", "Task 2", "Task 3"]
+
+    # Создаем временный файл с задачами
+    with open(todo_file, "w") as f:
+        json.dump(tasks_data, f)
+
+    # Выводим список задач
+    list_tasks(todo_file)
+
+    # Проверяем вывод
+    captured = capsys.readouterr()
+    assert captured.out == "1. Task 1\n2. Task 2\n3. Task 3\n"
+
+
+def test_remove_task_raises_index_error(todo_file):
+    """Test for the remove_task() function with IndexError."""
+    tasks_data = ["Task 1", "Task 2", "Task 3"]
+
+    with open(todo_file, "w") as f:
+        json.dump(tasks_data, f)
+
+    task_to_remove = 4
+
+    with pytest.raises(IndexError):
+        remove_task(task_to_remove, todo_file)
+
+
+if __name__ == "__main__":
+    pytest.main()
